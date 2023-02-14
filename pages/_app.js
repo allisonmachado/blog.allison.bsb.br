53dee3c56b64a6085005ee1c7ac8f427a9c39aa9
Allison Machado Gonçalves <aallison93@gmail.com>
2023-02-14 22:29:41 +0000

    feat: improve site css legibility

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { Analytics } from '@vercel/analytics/react';
 import '../styles/normalize.css'
 import '../styles/skeleton.css'
 import '../styles/global.css'
+import '../styles/custom.css'
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
